d28c4843b0dcc0d19ba556b79df8d6e2a503be69
YumaMatsumura <yumapine@gmail.com>
2023-01-31 12:56:06 +0900

    remove unnecessary package imports

--- launch/pass_through.launch.py
+++ launch/pass_through.launch.py
@@ -1,8 +1,7 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
