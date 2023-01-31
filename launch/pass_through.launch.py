fe80b20f82fe3f16b06905a5189241bdf6093244
YumaMatsumura <yumapine@gmail.com>
2023-01-31 18:32:48 +0900

    add rviz files

--- launch/pass_through.launch.py
+++ launch/pass_through.launch.py
@@ -1,13 +1,17 @@
 import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import Node, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
+    rviz_dir = os.path.join(get_package_share_directory('pcl_samples'), 'rviz')
+    rviz_file = os.path.join(rviz_dir, 'pass_through.rviz')
+    
     pass_through_node = ComposableNode(
         name='',
         package='pcl_samples',
@@ -24,5 +28,12 @@ def generate_launch_description():
                 pass_through_node
             ],
             output='screen'
+        ),
+        
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', rviz_file]
         )
     ])
